2ac9b7478777823f165a6ca9fe129707a9adb246
Cristina <cristina@ucan.be>
2022-06-13 08:25:51 +0200

    Console log dataabase

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -20,6 +20,8 @@ const parse = require("pg-connection-string").parse;
 const { host, port, database, user, password } = parse(
   process.env.DATABASE_URL
 );
+console.log(host);
+console.log(database);
 
 module.exports = () => ({
   connection: {
